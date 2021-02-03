23402f188ed9cc2a14b62f9ae1ff44c05d9cec79
Diogo Pinto <diogopintoes@gmail.com>
2021-02-03 21:32:08 +0000

    Calculating the Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,12 +8,23 @@ const burger = (props) => {
     // This basicaly does the following: It takes our array of ingredientes. Cycles through each Ingredient, and for each one it gets it's numeric value.
     // Then after receiving the numeric value it creates a new BurgerIngredient, based on the Name and Value it extracted.
     // 
-    const transformedIngredients = Object.keys(props.ingredients)       // Extracts keys from a object and turns it into an array. So it returns ['salad', 'bacon', 'cheese', 'meat'].                      
-        .map(ingredientName => {                                        // Map creates a new array. We execute a function for each ingredient.                        
-            return [...Array(props.ingredients[ingredientName])].map((_, i) => {    
+    console.log('Ingredients: ', props.ingredients);
+    let transformedIngredients = Object.keys(props.ingredients)         // Extracts keys from a object and turns it into an array. So it returns ['salad', 'bacon', 'cheese', 'meat'].                      
+        .map(ingredientName => {
+            return [...Array(props.ingredients[ingredientName])].map((_, i) => {
+                console.log('Key: ', ingredientName, 'Index', i);
                 return <BurgerIngredient key={ingredientName + i} type={ingredientName} />;
             });
-        });
+        })
+        .reduce((arr, el) => {                                          // Standart array function.
+            return arr.concat(el);
+        }, []);
+
+        if (transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients.</p>;
+        }
+
+    console.log('Transformed Ingredients: ', transformedIngredients);
 
     return (
         <div className={classes.Burger}>

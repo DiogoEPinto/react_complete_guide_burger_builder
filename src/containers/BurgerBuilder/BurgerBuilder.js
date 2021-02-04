300f8fcb8c2ac13030081f1ef19ce0e6e645c869
Diogo Pinto <diogopintoes@gmail.com>
2021-02-04 15:07:42 +0000

    Connecting State to Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,12 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 class BurgerBuilder extends Component {
 
     /* constructor(props){
@@ -17,14 +23,36 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const prevIngredientCount = this.state.ingredients[type];                       // Previous number of ingredients per type.
+        const updatedIngredientCount = prevIngredientCount + 1;                         // New number of ingredients per type.
+
+        const updatedIngredients = {                                                    // State Update: Copy old state information into new array.
+            ...this.state.ingredients                                                   // Spread operator
+        };
+        updatedIngredients[type] = updatedIngredientCount;                              // 
+        const priceAddition = INGREDIENT_PRICES[type]                                   // Price to add.
+        const prevPrice = this.state.totalPrice;                                        // Previous total price in state.
+        const newPrice = prevPrice + priceAddition;                                     // New total price.
+
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });       // Finally, update the state with the new information.
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render() {
         return (
             <Aux>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <Burger
+                    ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </Aux>
         );
     }

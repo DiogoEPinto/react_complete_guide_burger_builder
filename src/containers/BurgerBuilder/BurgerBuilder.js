efa076d5f656003649dca022b3fa8930c2dbb291
Diogo Pinto <diogopintoes@gmail.com>
2021-02-05 11:48:59 +0000

    Implementing the Button Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,6 +89,9 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -101,7 +104,10 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger
                     ingredients={this.state.ingredients} />
